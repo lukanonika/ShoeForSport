1f179e3e6bb2044be8f3daff8293eeaaa0487164
luka <spammluka@gmail.com>
2024-10-19 14:03:30 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function hider() {
+        
+    const info = document.getElementById('info');
+    const hideButton = document.getElementById('hideButton');
+    if (info.style.display === 'none') {
+      info.style.display = 'block';
+      hideButton.style.transform = "rotate(-180deg)";
+      
+    } else {
+      info.style.display = 'none';
+      hideButton.style.transform = "rotate(0deg)";     
+    }
+    }
+    
+
+    function hider1() {
+      
+      const info = document.getElementById('info1');
+      const hideButton = document.getElementById('hideButton1');
+      if (info.style.display === 'none') {
+        info.style.display = 'block';
+        hideButton.style.transform = "rotate(-180deg)";
+        
+      } else {
+        info.style.display = 'none';
+        hideButton.style.transform = "rotate(0deg)";
+        
+      }
+    }
+
+
+    function hider2() {
+      
+      const info = document.getElementById('info2');
+      const hideButton = document.getElementById('hideButton2');
+      if (info.style.display === 'none') {
+        info.style.display = 'block';
+        hideButton.style.transform = "rotate(-180deg)";
+        
+      } else {
+        info.style.display = 'none';
+        hideButton.style.transform = "rotate(0deg)";
+        
+      }
+    }
